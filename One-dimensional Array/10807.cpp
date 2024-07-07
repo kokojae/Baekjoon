9bfe9bb61579e6e19d63e3a1886cf53753a79505
Ko Min Jae <54296776+kokojae@users.noreply.github.com>
2024-07-07 12:03:48 +0900

    Create Repository

---
+++ One-dimensional Array/10807.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main1()
+{
+	int a, b[100] = { 0 }, c, d = 0;
+
+	std::cin >> a;
+	for (int i = 0; i < a; i++)
+		std::cin >> b[i];
+	std::cin >> c;
+
+	for (int i = 0; i < a; i++)
+		if (b[i] == c)
+			d++;
+
+	std::cout << d;
+
+	return 0;
+}
