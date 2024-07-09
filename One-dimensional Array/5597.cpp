5c988e2aceeb8149456f82ecafbf41debb0577e0
Ko Min Jae <54296776+kokojae@users.noreply.github.com>
2024-07-09 15:37:44 +0900

    Q5597

--- One-dimensional Array/5597.cpp
+++ One-dimensional Array/5597.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
-int main()
+int Q5597()
 {
+	int a[30] = { 0 }, b;
+	for (int i = 0; i < 28; i++)
+	{
+		std::cin >> b;
+		a[b - 1] = b;
+	}
+	for (int i = 0; i < 30; i++)
+	{
+		if (a[i] == 0)
+			std::cout << (i + 1) << std::endl;
+	}
 
 	return 0;
 }
